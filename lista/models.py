b529b9ca1bb9f3088850a86e3a2c22399a9db6b9
r4t0m4n0 <r4t0m4n0@protonmail.com>
2024-08-25 14:48:33 -0300

    FIX: ordem de tabelas incorreta, agora equipamente deve existir antes de fazer um agendamento

--- lista/models.py
+++ lista/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
-class Agendamento(models.Model):
-    prof_instrutor = models.CharField(max_length=40)
-    sala = models.CharField(max_length=20)
-    turno = models.CharField(max_length=5)
-    data = models.DateField()
-
 class Equipamentos(models.Model):
-    agendamento = models.OneToOneField(Agendamento, related_name="agendamento", on_delete=models.CASCADE, primary_key=True)
     notebook = models.IntegerField()
     projetor = models.IntegerField()
     camera_fotorgrafica = models.IntegerField()
@@ -17,3 +10,10 @@ class Equipamentos(models.Model):
     caneta_lousa_interativa = models.IntegerField()
     controle_tv = models.IntegerField()
     controle_projetor = models.IntegerField()
+
+class Agendamento(models.Model):
+    equipamentos = models.OneToOneField(Equipamentos, related_name="equipamentos", on_delete=models.CASCADE, primary_key=True)
+    prof_instrutor = models.CharField(max_length=40)
+    sala = models.CharField(max_length=20)
+    turno = models.CharField(max_length=5)
+    data = models.DateField()
